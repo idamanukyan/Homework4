5e5d0d03e110c4f4b38fd1d34fe38d59129436af
idamanukyan <ida_manukyan@edu.aua.am>
2023-12-10 18:00:18 +0400

    added unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,11 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.robolectric:robolectric:4.7.1")
+    testImplementation ("io.kotlintest:kotlintest-runner-junit5:4.6.2")
+    testImplementation ("io.kotlintest:kotlintest-assertions:4.6.2")
+    testImplementation ("io.mockk:mockk:1.12.0")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/test/java/com/example/homework4/ExampleUnitTest.kt
+++ app/src/test/java/com/example/homework4/ExampleUnitTest.kt
@@ -1,17 +1,103 @@
 package com.example.homework4
 
-import org.junit.Test
-
+import io.mockk.coEvery
+import io.mockk.mockk
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    private val mockWeatherApiService = mockk<WeatherApiService>()
+    private val mockLocationPermissionRequester = mockk<MainActivity.LocationPermissionRequester>()
+    private val mockNavController = mockk<NavHostController>()
+
+    @Test
+    fun testWelcomeScreen() {
+        val mockWeatherInfo = "Mock Weather Info"
+        coEvery { mockLocationPermissionRequester.requestLocationPermission(any()) } coAnswers {
+            secondArg<(String) -> Unit>().invoke(mockWeatherInfo)
+        }
+
+        val locationPermissionViewModel = MainActivity.LocationPermissionViewModel()
+
+        WelcomeScreen(
+            navController = mockNavController,
+            locationPermissionRequester = mockLocationPermissionRequester,
+            weatherApiService = mockWeatherApiService,
+            temperatureUnit = TemperatureUnit.Celsius,
+            onTemperatureUnitChanged = { }
+        )
+
+
+        coEvery { mockWeatherApiService.getWeatherForCity(any()) } coAnswers {
+            mockk<WeatherResponse>()
+        }
+
+        @Test
+        fun testLocationPermissionRequester() {
+            val mockWeatherInfo = "Mock Weather Info"
+            coEvery { mockLocationPermissionRequester.requestLocationPermission(any()) } coAnswers {
+                secondArg<(String) -> Unit>().invoke(mockWeatherInfo)
+            }
+
+            val locationPermissionViewModel = MainActivity.LocationPermissionViewModel()
+
+
+            val locationPermissionRequester =
+                MainActivity.LocationPermissionRequester(
+                    mockNavController,
+                    locationPermissionViewModel
+                )
+        }
+
+        @Test
+        fun testLocationPermissionViewModel() {
+            val locationPermissionViewModel = MainActivity.LocationPermissionViewModel()
+            assert(!locationPermissionViewModel.isPermissionRequested)
+            locationPermissionViewModel.onPermissionRequested()
+            assert(locationPermissionViewModel.isPermissionRequested)
+        }
+
+        @Test
+        fun testWeatherApiService() {
+            val cityName = "Berlin"
+            val mockWeatherResponse = mockk<WeatherResponse>()
+            coEvery { mockWeatherApiService.getWeatherForCity(cityName) } returns mockWeatherResponse
+        }
+
+        @Test
+        fun testNavigation() {
+            val cityName = "Berlin"
+            val navController = rememberNavController()
+            navController.navigate("second_screen/$cityName")
+        }
+
+        @Test
+        fun testWelcomeScreenUI() {
+            val mockWeatherInfo = "Mock Weather Info"
+            val locationPermissionViewModel = MainActivity.LocationPermissionViewModel()
+
+            WelcomeScreen(
+                navController = mockNavController,
+                locationPermissionRequester = mockLocationPermissionRequester,
+                weatherApiService = mockWeatherApiService,
+                temperatureUnit = TemperatureUnit.Celsius,
+                onTemperatureUnitChanged = {}
+            )
+        }
+
+
+    }
+
 }
